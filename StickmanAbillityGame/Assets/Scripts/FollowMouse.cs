5321a7d9a8e975983478e0f7a326e8158899f36f
vionisy <alvin@dobric.de>
2021-05-15 21:18:52 +0200

    PARTICLES
    dsa

--- StickmanAbillityGame/Assets/Scripts/FollowMouse.cs
+++ StickmanAbillityGame/Assets/Scripts/FollowMouse.cs
@@ -13,11 +13,24 @@ public class FollowMouse : MonoBehaviour
     {
         cam = FindObjectOfType<Camera>();
     }
-    
+
 
     void Update()
     {
-        
+
+        if (Time.timeScale == 0.5)
+        {
+            speed = 5;
+        }
+        else if (Time.timeScale == 1)
+        {
+            speed = 300;
+        }
+        else if (Time.timeScale != 1 && Time.timeScale != 0.5)
+        {
+            speed = 2;
+        }
+
         Vector3 playerpos = new Vector3(cam.ScreenToWorldPoint(Input.mousePosition).x, cam.ScreenToWorldPoint(Input.mousePosition).y, 90);
         Vector3 difference = playerpos - transform.position;
         float rotationZ = Mathf.Atan2(difference.x, -difference.y) * Mathf.Rad2Deg;
