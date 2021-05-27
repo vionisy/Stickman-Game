cf555d384e64c446b78e25b8b2db012e1e871aee
BlackPearl00717 <anton.ploetner1@gmail.com>
2021-05-27 15:59:50 +0200

    KnockBack and Camera Shake

--- StickmanAbillityGame/Assets/damage.cs
+++ StickmanAbillityGame/Assets/damage.cs
@@ -11,7 +11,11 @@ public class damage : MonoBehaviour
     private float speed;
     private Vector3 oldPosition;
     public PhotonView photonView;
-
+    private StressReceiver camerashake;
+    private void Start()
+    {
+        camerashake = FindObjectOfType<Camera>().GetComponent<StressReceiver>();
+    }
     void FixedUpdate()
     {
         speed = Vector3.Distance(oldPosition, transform.position) * 100f;
@@ -22,25 +26,35 @@ public class damage : MonoBehaviour
     {
         if (speed >= 10)
         {
-            if (Input.GetKey(mousebutton) && collision.gameObject.tag == "Head" || collision.gameObject.tag == "Chest")
+            if (Input.GetKey(mousebutton)) //&& collision.gameObject.tag == "Head" || collision.gameObject.tag == "Chest")
             {
                 speed -= 10;
-                collision.gameObject.GetComponentInParent<PlayerController>().Damage(speed * 0.5f);
-                photonView.RPC("knockback", PhotonTargets.Others);
+                if (speed >= 35)
+                {
+                    Debug.Log("CameraShake");
+                    StartCoroutine("CameraShake");
+                    speed += 10;
+                }
+                collision.gameObject.GetComponentInParent<PlayerController>().Damage(speed * 0.4f);
+                photonView.RPC("knockback", PhotonTargets.Others, speed);
             }
         }
     }
-
+    private IEnumerator CameraShake()
+    {
+        camerashake.InduceStress(1);
+        yield return new WaitForSeconds(0.1f);
+        camerashake.InduceStress(0);
+    }
     [PunRPC]
-    public void knockback()
+    public void knockback(float speed)
     {
         Collider2D[] objects = Physics2D.OverlapCircleAll(transform.position, fieldofImpact, LayerToHit);
         foreach (Collider2D obj in objects)
         {
-
             Debug.Log("Knockback");
             Vector2 direction = obj.transform.position - transform.position;
-            obj.GetComponent<Rigidbody2D>().AddForce(direction * force);
+            obj.GetComponent<Rigidbody2D>().AddForce(direction * (speed * 4));
         }
     }
 }
