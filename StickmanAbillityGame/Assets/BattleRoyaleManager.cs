7363c77ccbd06ae8b046370e76c7857872cc4e6c
BlackPearl00717 <anton.ploetner1@gmail.com>
2021-06-30 16:20:43 +0200

    Random Spawnpoints

--- StickmanAbillityGame/Assets/BattleRoyaleManager.cs
+++ StickmanAbillityGame/Assets/BattleRoyaleManager.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class BattleRoyaleManager : MonoBehaviour
 {
+    private bool foundOne = false;
+    private Vector3 SpawnPosition;
+    public List<spawnpoint> Spawnpoints;
     public GameObject HandyCanvas;
     public GameObject PlayerPreafab;
     public GameObject SceneCamera;
@@ -14,6 +17,7 @@ public class BattleRoyaleManager : MonoBehaviour
     public static bool HandyControllsOn = false;
     static public bool E_pressed;
     static public bool Q_pressed;
+    public PhotonView photonView;
     private void Update()
     {
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
@@ -34,15 +38,24 @@ public class BattleRoyaleManager : MonoBehaviour
 
     public void SpawnPlayer()
     {
-        float randomValue = Random.Range(-50f, 50f);
-        PhotonNetwork.Instantiate(PlayerPreafab.name, new Vector2(this.transform.position.x + randomValue, this.transform.position.y), Quaternion.identity, 0);
+        int randmOne = Random.Range(0, Spawnpoints.Count);
+        SpawnPosition = Spawnpoints[randmOne].transform.position;
+        photonView.RPC("spawned", PhotonTargets.AllBuffered, randmOne);
+        float randomValue = Random.Range(-150f, 150f);
+        PhotonNetwork.Instantiate(PlayerPreafab.name, new Vector2(SpawnPosition.x + randomValue, SpawnPosition.y), Quaternion.identity, 0);
         SceneCamera.SetActive(true);
+    }
+    [PunRPC]
+    public void spawned(int pos)
+    {
+        Debug.Log("Removed");
+        Spawnpoints.RemoveAt(pos);
     }
     public IEnumerator Respawn()
     {
         Debug.Log("Respawn");
         yield return new WaitForSeconds(respwanTime);
-        float randomValue = Random.Range(-50f, 50f);
+        float randomValue = Random.Range(-150f, 500f);
         PhotonNetwork.Instantiate(PlayerPreafab.name, new Vector2(this.transform.position.x + randomValue, this.transform.position.y), Quaternion.identity, 0);
     }
     public void reset()
